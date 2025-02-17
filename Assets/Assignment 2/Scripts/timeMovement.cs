f537588fde970822c8326c4064022c79f2510585
OUTOFTHEBLUE\whate <whateverfloatsyourboat077@gmail.com>
2025-02-17 15:50:41 -0500

    Script to move objects such as the moon and sun.

---
+++ Assets/Assignment 2/Scripts/timeMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.PlayerSettings;
+
+public class timeMovement : MonoBehaviour
+{
+    public GameObject Sun;
+    public GameObject Moon;
+    public Slider position;
+    
+    void Update()
+    {
+        Vector3 sunpos = Sun.transform.position;
+        Vector3 moonpos = Moon.transform.position;
+        sunpos.x = position.value - 5;
+        moonpos.x = position.value - 19;
+        Sun.transform.position = sunpos;
+        Moon.transform.position = moonpos;
+    }
+
+    
+    
+}
