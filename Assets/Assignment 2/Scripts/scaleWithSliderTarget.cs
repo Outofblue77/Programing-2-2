97eef77a1076f1d5d046c80df35943dc420dd764
OUTOFTHEBLUE\whate <whateverfloatsyourboat077@gmail.com>
2025-02-17 19:34:32 -0500

    Added Size to change target distance.

---
+++ Assets/Assignment 2/Scripts/scaleWithSliderTarget.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class scaleWithSliderTarget : MonoBehaviour
+{
+    public UnityEngine.UI.Slider scaleTarget;
+    public GameObject target;
+
+    float scaleValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleValue = scaleTarget.value;
+        Vector3 size = new Vector3(scaleValue, scaleValue, scaleValue);           
+        target.transform.localScale = size;
+        
+    }
+}
